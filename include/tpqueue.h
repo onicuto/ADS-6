a637a09514fcd77e442847ee18d7453ad9791c62
onicuto <kiryauvachan@gmail.com>
2026-05-12 16:07:33 +0300

    ADS-6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,66 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
+#ifndef TPQUEUE_H
+#define TPQUEUE_H
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+#include <cstddef>
+
+template <typename T> class TPQueue {
+private:
+  struct Node {
+    T data;
+    Node *next;
+
+    Node(const T &d) : data(d), next(nullptr) {}
+  };
+
+  Node *head;
+
+public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (head) {
+      Node *temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  TPQueue(const TPQueue &) = delete;
+  TPQueue &operator=(const TPQueue &) = delete;
+
+  void push(const T &item) {
+    Node *newNode = new Node(item);
+
+    if (!head || item.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      return;
+    }
+
+    Node *current = head;
+    while (current->next && current->next->data.prior >= item.prior) {
+      current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+  }
+
+  T pop() {
+    if (!head) {
+      return T{};
+    }
+
+    Node *temp = head;
+    T result = head->data;
+    head = head->next;
+    delete temp;
+
+    return result;
+  }
+
+  bool empty() const { return head == nullptr; }
 };
 
-struct SYM {
-  char ch;
-  int prior;
-};
-
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // TPQUEUE_H
